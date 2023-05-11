e84cbda1cdcb576c8d944609a5d51a0248b8fd3c
suellenac <suellen.cola@gmail.com>
2023-05-10 22:47:37 -0300

    Updated build.gradle to use compose latest version and material design 3. Updated the app theme to use materialDesign 3

--- app/src/main/java/com/suellen/colangelo/notes/ui/theme/Theme.kt
+++ app/src/main/java/com/suellen/colangelo/notes/ui/theme/Theme.kt
@@ -1,31 +1,14 @@
 package com.suellen.colangelo.notes.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-        primary = Purple200,
-        primaryVariant = Purple700,
-        secondary = Teal200
-)
+private val DarkColorPalette = darkColorScheme()
 
-private val LightColorPalette = lightColors(
-        primary = Purple500,
-        primaryVariant = Purple700,
-        secondary = Teal200
-
-        /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
+private val LightColorPalette = lightColorScheme()
 
 @Composable
 fun NotesTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
@@ -36,9 +19,9 @@ fun NotesTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable 
     }
 
     MaterialTheme(
-            colors = colors,
-            typography = Typography,
-            shapes = Shapes,
-            content = content
+        colorScheme = colors,
+        typography = Typography,
+        shapes = AppShapes,
+        content = content
     )
 }
